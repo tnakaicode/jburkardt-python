a8c181260284296c3b96a8ee5b5f02497c0cc84e
Taku Nakai <t.nakai.office@gmail.com>
2020-03-01 21:06:30 +0900

    euler

--- mario/mario.py
+++ mario/mario.py
@@ -1,30 +1,32 @@
 #! /usr/bin/env python3
 #
-def mario ( ):
 
-#*****************************************************************************80
-#
-## MARIO creates an image of Mario using colored squares.
-#
-#  Licensing:
-#
-#    This code is distributed under the GNU LGPL license.
-#
-#  Modified:
-#
-#    19 April 2018
-#
-#  Author:
-#
-#    John Burkardt
-#
-  import matplotlib.pyplot as plt
-  import numpy as np
 
-  print ( '' )
-  print ( 'MARIO:' )
-  print ( '  Python version' )
-  print ( '  Draw a picture of Mario, using colored squares.' )
+def mario():
+
+    # *****************************************************************************80
+    #
+    # MARIO creates an image of Mario using colored squares.
+    #
+    #  Licensing:
+    #
+    #    This code is distributed under the GNU LGPL license.
+    #
+    #  Modified:
+    #
+    #    19 April 2018
+    #
+    #  Author:
+    #
+    #    John Burkardt
+    #
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    print('')
+    print('MARIO:')
+    print('  Python version')
+    print('  Draw a picture of Mario, using colored squares.')
 #
 #  Color indices:
 #
@@ -36,31 +38,31 @@ def mario ( ):
 #  5: beige
 #  6: brown
 #
-  color_index = np.array ( [ \
-    [ 0, 0, 0, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0 ], \
-    [ 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0 ], \
-    [ 0, 0, 6, 6, 6, 5, 5, 5, 1, 5, 0, 0, 0 ], \
-    [ 0, 6, 5, 6, 5, 5, 5, 5, 1, 5, 5, 5, 0 ], \
-    [ 0, 6, 5, 6, 6, 5, 5, 5, 5, 1, 5, 5, 5 ], \
-    [ 0, 6, 6, 5, 5, 5, 5, 5, 1, 1, 1, 1, 0 ], \
-    [ 0, 0, 0, 5, 5, 5, 5, 5, 5, 5, 5, 0, 0 ], \
-    [ 0, 0, 2, 2, 3, 2, 2, 2, 2, 0, 0, 0, 0 ], \
-    [ 0, 2, 2, 2, 3, 2, 2, 3, 2, 2, 2, 0, 0 ], \
-    [ 2, 2, 2, 2, 3, 3, 3, 3, 2, 2, 2, 2, 0 ], \
-    [ 5, 5, 2, 3, 4, 3, 3, 4, 3, 2, 5, 5, 0 ], \
-    [ 5, 5, 5, 3, 3, 3, 3, 3, 3, 5, 5, 5, 0 ], \
-    [ 5, 5, 3, 3, 3, 3, 3, 3, 3, 3, 5, 5, 0 ], \
-    [ 0, 0, 3, 3, 3, 0, 0, 3, 3, 3, 0, 0, 0 ], \
-    [ 0, 6, 6, 6, 0, 0, 0, 0, 6, 6, 6, 0, 0 ], \
-    [ 6, 6, 6, 6, 0, 0, 0, 0, 6, 6, 6, 6, 0 ] ] )
+    color_index = np.array([
+        [0, 0, 0, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0],
+        [0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0],
+        [0, 0, 6, 6, 6, 5, 5, 5, 1, 5, 0, 0, 0],
+        [0, 6, 5, 6, 5, 5, 5, 5, 1, 5, 5, 5, 0],
+        [0, 6, 5, 6, 6, 5, 5, 5, 5, 1, 5, 5, 5],
+        [0, 6, 6, 5, 5, 5, 5, 5, 1, 1, 1, 1, 0],
+        [0, 0, 0, 5, 5, 5, 5, 5, 5, 5, 5, 0, 0],
+        [0, 0, 2, 2, 3, 2, 2, 2, 2, 0, 0, 0, 0],
+        [0, 2, 2, 2, 3, 2, 2, 3, 2, 2, 2, 0, 0],
+        [2, 2, 2, 2, 3, 3, 3, 3, 2, 2, 2, 2, 0],
+        [5, 5, 2, 3, 4, 3, 3, 4, 3, 2, 5, 5, 0],
+        [5, 5, 5, 3, 3, 3, 3, 3, 3, 5, 5, 5, 0],
+        [5, 5, 3, 3, 3, 3, 3, 3, 3, 3, 5, 5, 0],
+        [0, 0, 3, 3, 3, 0, 0, 3, 3, 3, 0, 0, 0],
+        [0, 6, 6, 6, 0, 0, 0, 0, 6, 6, 6, 0, 0],
+        [6, 6, 6, 6, 0, 0, 0, 0, 6, 6, 6, 6, 0]])
 
-  dims = color_index.shape
-  color_index_m = dims[0]
-  color_index_n = dims[1]
+    dims = color_index.shape
+    color_index_m = dims[0]
+    color_index_n = dims[1]
 
-  grid_m = color_index_m
-  grid_n = color_index_n
-  grid = np.zeros ( [ grid_m, grid_n ] )
+    grid_m = color_index_m
+    grid_n = color_index_n
+    grid = np.zeros([grid_m, grid_n])
 #
 #  Fill the I,J box with color K.
 #  Actually, "I" becomes M-I+1, because matrix row 1 is at the top,
@@ -74,124 +76,127 @@ def mario ( ):
 #                        |
 #  (m-i+1,j-1) ----> (m-i+1,j)
 #
-  plt.axis ( 'equal' )
-  plt.axis ( 'off' )
+    plt.axis('equal')
+    plt.axis('off')
 
-  for i in range ( 0, grid_m ):
-    for j in range ( 0, grid_n ):
+    for i in range(0, grid_m):
+        for j in range(0, grid_n):
 
-      k = color_index[i,j]
+            k = color_index[i, j]
 
-      if ( k == 0 ):
-        color = 'white'
-      elif ( k == 1 ):
-        color = 'black'
-      elif ( k == 2 ):
-        color = 'red'
-      elif ( k == 3 ):
-        color = 'blue'
-      elif ( k == 4 ):
-        color = 'yellow'
-      elif ( k == 5 ):
-        color = 'bisque'
-      elif ( k == 6 ):
-        color = 'brown'
+            if (k == 0):
+                color = 'white'
+            elif (k == 1):
+                color = 'black'
+            elif (k == 2):
+                color = 'red'
+            elif (k == 3):
+                color = 'blue'
+            elif (k == 4):
+                color = 'yellow'
+            elif (k == 5):
+                color = 'bisque'
+            elif (k == 6):
+                color = 'brown'
 
-      cell_ij_fill ( grid_m, i, j, color )
+            cell_ij_fill(grid_m, i, j, color)
 
-  filename = 'mario.png'
-  plt.savefig ( filename )
-  print ( '  Graphics saved as ', filename )
+    filename = 'mario.png'
+    plt.savefig(filename)
+    print('  Graphics saved as ', filename)
 
 # plt.show ( )
-  plt.clf ( )
+    plt.clf()
 #
 #  Terminate.
 #
-  print ( '' )
-  print ( 'MARIO:' )
-  print ( '  Normal end of execution.' )
+    print('')
+    print('MARIO:')
+    print('  Normal end of execution.')
 
-  return
+    return
 
-def cell_ij_fill ( m, i, j, color ):
 
-#*****************************************************************************80
-#
-## CELL_IJ_FILL plots a filled (I,J) cell.
-#
-#  Discussion:
-#
-#    We assume the data is represented in a matrix.
-#
-#    In order to convert between the matrix coordinates and picture
-#    coordinates, the (I,J) cell will be drawn with the following corners:
-#
-#    (j-1,m-i+1), (j,m-i+1), (j,m-i), (j-1,m-1).
-#
-#  Licensing:
-#
-#    This code is distributed under the GNU LGPL license.
-#
-#  Modified:
-#
-#    18 April 2018
-#
-#  Author:
-#
-#    John Burkardt
-#
-#  Parameters:
-#
-#    Input, integer M, the maximum row index.
-#
-#    Input, integer I, J, the index of the cell.
-#
-#    Input, MATLAB color COLOR, can be any of the 8 abbreviated color terms
-#    'r', 'g', 'b', 'c', 'm', 'y', 'w', 'k', or an RGB triple such as 
-#    [1.0,0.4,0.0].  The square is filled with this color.
-#
-  import matplotlib.pyplot as plt
+def cell_ij_fill(m, i, j, color):
 
-  a = j - 1
-  b = j
-  c = m - ( i - 1 )
-  d = m - i
+    # *****************************************************************************80
+    #
+    # CELL_IJ_FILL plots a filled (I,J) cell.
+    #
+    #  Discussion:
+    #
+    #    We assume the data is represented in a matrix.
+    #
+    #    In order to convert between the matrix coordinates and picture
+    #    coordinates, the (I,J) cell will be drawn with the following corners:
+    #
+    #    (j-1,m-i+1), (j,m-i+1), (j,m-i), (j-1,m-1).
+    #
+    #  Licensing:
+    #
+    #    This code is distributed under the GNU LGPL license.
+    #
+    #  Modified:
+    #
+    #    18 April 2018
+    #
+    #  Author:
+    #
+    #    John Burkardt
+    #
+    #  Parameters:
+    #
+    #    Input, integer M, the maximum row index.
+    #
+    #    Input, integer I, J, the index of the cell.
+    #
+    #    Input, MATLAB color COLOR, can be any of the 8 abbreviated color terms
+    #    'r', 'g', 'b', 'c', 'm', 'y', 'w', 'k', or an RGB triple such as
+    #    [1.0,0.4,0.0].  The square is filled with this color.
+    #
+    import matplotlib.pyplot as plt
 
-  plt.fill ( [ a, b, b, a ], [ c, c, d, d ], color )
+    a = j - 1
+    b = j
+    c = m - (i - 1)
+    d = m - i
 
-  return
+    plt.fill([a, b, b, a], [c, c, d, d], color)
 
-def timestamp ( ):
+    return
 
-#*****************************************************************************80
-#
-## TIMESTAMP prints the date as a timestamp.
-#
-#  Licensing:
-#
-#    This code is distributed under the GNU LGPL license. 
-#
-#  Modified:
-#
-#    06 April 2013
-#
-#  Author:
-#
-#    John Burkardt
-#
-#  Parameters:
-#
-#    None
-#
-  import time
 
-  t = time.time ( )
-  print ( time.ctime ( t ) )
+def timestamp():
 
-  return None
+    # *****************************************************************************80
+    #
+    # TIMESTAMP prints the date as a timestamp.
+    #
+    #  Licensing:
+    #
+    #    This code is distributed under the GNU LGPL license.
+    #
+    #  Modified:
+    #
+    #    06 April 2013
+    #
+    #  Author:
+    #
+    #    John Burkardt
+    #
+    #  Parameters:
+    #
+    #    None
+    #
+    import time
 
-if ( __name__ == '__main__' ):
-  timestamp ( )
-  mario ( )
-  timestamp ( )
+    t = time.time()
+    print(time.ctime(t))
+
+    return None
+
+
+if (__name__ == '__main__'):
+    timestamp()
+    mario()
+    timestamp()
--- ode_euler/ode_euler.py
+++ ode_euler/ode_euler.py
@@ -1,212 +1,218 @@
 #! /usr/bin/env python3
 #
-def euler ( dydt, tspan, y0, n ):
 
-#*****************************************************************************80
-#
-## euler approximates an ODE solution using Euler's method.
-#
-#  Licensing:
-#
-#    This code is distributed under the GNU LGPL license.
-#
-#  Modified:
-#
-#    30 January 2020
-#
-#  Author:
-#
-#    John Burkardt
-#
-#  Input:
-#
-#    function dydt: the right hand side of the differential equation.
-#
-#    real tspan[2]: the starting and ending points.
-#
-#    real y0: the initial condition. 
-#
-#    integer n: the number of steps to take.
-#
-#  Output:
-#
-#    real t(n+1), y(n+1): the sequence of solution estimates.
-#
-  import numpy as np
 
-  t0 = tspan[0]
-  t1 = tspan[1]
-  dt = ( t1 - t0 ) / float ( n )
-  t = np.linspace ( t0, t1, n + 1 );
-  y = np.zeros ( n + 1 );
-  y[0] = y0;
-  for i in range ( n ):
-    y[i+1] = y[i] + dt * dydt ( t[i], y[i] )
+def euler(dydt, tspan, y0, n):
 
-  return t, y
+    # *****************************************************************************80
+    #
+    # euler approximates an ODE solution using Euler's method.
+    #
+    #  Licensing:
+    #
+    #    This code is distributed under the GNU LGPL license.
+    #
+    #  Modified:
+    #
+    #    30 January 2020
+    #
+    #  Author:
+    #
+    #    John Burkardt
+    #
+    #  Input:
+    #
+    #    function dydt: the right hand side of the differential equation.
+    #
+    #    real tspan[2]: the starting and ending points.
+    #
+    #    real y0: the initial condition.
+    #
+    #    integer n: the number of steps to take.
+    #
+    #  Output:
+    #
+    #    real t(n+1), y(n+1): the sequence of solution estimates.
+    #
+    import numpy as np
 
-def humps_deriv ( x, y ):
+    t0 = tspan[0]
+    t1 = tspan[1]
+    dt = (t1 - t0) / float(n)
+    t = np.linspace(t0, t1, n + 1)
+    y = np.zeros(n + 1)
+    y[0] = y0
+    for i in range(n):
+        y[i + 1] = y[i] + dt * dydt(t[i], y[i])
 
-#*****************************************************************************80
-#
-## humps_deriv evaluates the derivative of the humps def for an ODE solver.
-#
-#  Discussion:
-#
-#    This verion of "humps_deriv" appends the input argument "y", as expected 
-#    by most ODE solving software.
-#
-#  Licensing:
-#
-#    This code is distributed under the GNU LGPL license.
-#
-#  Modified:
-#
-#    06 August 2019
-#
-#  Author:
-#
-#    John Burkardt
-#
-#  Input:
-#
-#    real x(): the arguments.
-#
-#    real y(): the value of the humps function.
-#
-#  Output:
-#
-#    real yp(): the value of the derivative of the humps function.
-#
-  yp = - 1.0 / ( ( x - 0.3 )**2 + 0.01 )**2 \
-      * 2.0 * ( x - 0.3 ) \
-      - 1.0 / ( ( x - 0.9 )**2 + 0.04 )**2 \
-      * 2.0 * ( x - 0.9 )
+    return t, y
 
-  return yp
 
-def humps_euler ( ):
+def humps_deriv(x, y):
 
-#*****************************************************************************80
-#
-## humps_euler ???
-#
-#  Licensing:
-#
-#    This code is distributed under the GNU LGPL license.
-#
-#  Modified:
-#
-#    06 August 2019
-#
-#  Author:
-#
-#    John Burkardt
-#
-  import matplotlib.pyplot as plt
-  import numpy as np
-  import platform
+    # *****************************************************************************80
+    #
+    # humps_deriv evaluates the derivative of the humps def for an ODE solver.
+    #
+    #  Discussion:
+    #
+    #    This verion of "humps_deriv" appends the input argument "y", as expected
+    #    by most ODE solving software.
+    #
+    #  Licensing:
+    #
+    #    This code is distributed under the GNU LGPL license.
+    #
+    #  Modified:
+    #
+    #    06 August 2019
+    #
+    #  Author:
+    #
+    #    John Burkardt
+    #
+    #  Input:
+    #
+    #    real x(): the arguments.
+    #
+    #    real y(): the value of the humps function.
+    #
+    #  Output:
+    #
+    #    real yp(): the value of the derivative of the humps function.
+    #
+    yp = - 1.0 / ((x - 0.3)**2 + 0.01)**2 \
+        * 2.0 * (x - 0.3) \
+        - 1.0 / ((x - 0.9)**2 + 0.04)**2 \
+        * 2.0 * (x - 0.9)
 
-  print ( '' )
-  print ( 'humps_euler' )
-  print ( '  Python version: %s' % ( platform.python_version ( ) ) )
-  print ( '  Test the euler ode integrator on the humps problem.' )
+    return yp
 
-  t0 = 0.0
-  t1 = 2.0
-  tspan = np.array ( [ t0, t1 ] )
-  y0 = humps_fun ( t0 )
-  n = 100
 
-  [ t, y ] = euler ( humps_deriv, tspan, y0, n )
+def humps_euler():
 
-  plt.clf ( )
+    # *****************************************************************************80
+    #
+    # humps_euler ???
+    #
+    #  Licensing:
+    #
+    #    This code is distributed under the GNU LGPL license.
+    #
+    #  Modified:
+    #
+    #    06 August 2019
+    #
+    #  Author:
+    #
+    #    John Burkardt
+    #
+    import matplotlib.pyplot as plt
+    import numpy as np
+    import platform
 
-  plt.plot ( t, y, 'b-', linewidth = 2 )
+    print('')
+    print('humps_euler')
+    print('  Python version: %s' % (platform.python_version()))
+    print('  Test the euler ode integrator on the humps problem.')
 
-  if ( t0 <= 0.0 and 0.0 <= t1 ):
-    plt.plot ( [t0,t1], [0,0], 'k-', linewidth = 2 )
+    t0 = 0.0
+    t1 = 2.0
+    tspan = np.array([t0, t1])
+    y0 = humps_fun(t0)
+    n = 100
 
-  ymin = min ( y )
-  ymax = max ( y )
-  if ( ymin <= 0.0 and 0.0 <= ymax ):
-    plt.plot ( [0, 0], [ymin,ymax], 'k-', linewidth = 2 )
+    [t, y] = euler(humps_deriv, tspan, y0, n)
 
-  plt.grid ( True )
-  plt.xlabel ( '<--- t --->' )
-  plt.ylabel ( '<--- y(t) --->' )
-  plt.title ( 'y = humps(t)' )
-  filename = 'humps_euler.png'
-  plt.savefig ( filename )
-  print ( '  Graphics saved as "%s"' % ( filename ) )
+    plt.clf()
+
+    plt.plot(t, y, 'b-', linewidth=2)
+
+    if (t0 <= 0.0 and 0.0 <= t1):
+        plt.plot([t0, t1], [0, 0], 'k-', linewidth=2)
+
+    ymin = min(y)
+    ymax = max(y)
+    if (ymin <= 0.0 and 0.0 <= ymax):
+        plt.plot([0, 0], [ymin, ymax], 'k-', linewidth=2)
+
+    plt.grid(True)
+    plt.xlabel('<--- t --->')
+    plt.ylabel('<--- y(t) --->')
+    plt.title('y = humps(t)')
+    filename = 'humps_euler.png'
+    plt.savefig(filename)
+    print('  Graphics saved as "%s"' % (filename))
 #
 #  Terminate.
 #
-  print ( '' )
-  print ( 'humps_euler_test:' )
-  print ( '  Normal end of execution.' )
+    print('')
+    print('humps_euler_test:')
+    print('  Normal end of execution.')
 
-  return
+    return
 
-def humps_fun ( x ):
 
-#*****************************************************************************80
-#
-## humps_fun evaluates the humps function.
-#
-#  Licensing:
-#
-#    This code is distributed under the GNU LGPL license.
-#
-#  Modified:
-#
-#    06 August 2019
-#
-#  Author:
-#
-#    John Burkardt
-#
-#  Input:
-#
-#    real x(): the evaluation points.
-#
-#  Output:
-#
-#    real y(): the def values.
-#
-  y = 1.0 / ( ( x - 0.3 )**2 + 0.01 ) \
-    + 1.0 / ( ( x - 0.9 )**2 + 0.04 ) \
-    - 6.0
+def humps_fun(x):
 
-  return y
+    # *****************************************************************************80
+    #
+    # humps_fun evaluates the humps function.
+    #
+    #  Licensing:
+    #
+    #    This code is distributed under the GNU LGPL license.
+    #
+    #  Modified:
+    #
+    #    06 August 2019
+    #
+    #  Author:
+    #
+    #    John Burkardt
+    #
+    #  Input:
+    #
+    #    real x(): the evaluation points.
+    #
+    #  Output:
+    #
+    #    real y(): the def values.
+    #
+    y = 1.0 / ((x - 0.3)**2 + 0.01) \
+        + 1.0 / ((x - 0.9)**2 + 0.04) \
+        - 6.0
 
-def timestamp ( ):
+    return y
 
-#*****************************************************************************80
-#
-## timestamp prints the date as a timestamp.
-#
-#  Licensing:
-#
-#    This code is distributed under the GNU LGPL license. 
-#
-#  Modified:
-#
-#    21 August 2019
-#
-#  Author:
-#
-#    John Burkardt
-#
-  import time
 
-  t = time.time ( )
-  print ( time.ctime ( t ) )
+def timestamp():
 
-  return
+    # *****************************************************************************80
+    #
+    # timestamp prints the date as a timestamp.
+    #
+    #  Licensing:
+    #
+    #    This code is distributed under the GNU LGPL license.
+    #
+    #  Modified:
+    #
+    #    21 August 2019
+    #
+    #  Author:
+    #
+    #    John Burkardt
+    #
+    import time
 
-if ( __name__ == '__main__' ):
-  timestamp ( )
-  humps_euler ( )
-  timestamp ( )
+    t = time.time()
+    print(time.ctime(t))
 
+    return
+
+
+if (__name__ == '__main__'):
+    timestamp()
+    humps_euler()
+    timestamp()
