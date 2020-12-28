1d953399d20885f440550609a8568a704838b992
tnakaicode <tnakaicode@gmail.com>
2020-12-29 02:12:03 +0900

    brownian

--- brownian_motion_simulation/brownian.py
+++ brownian_motion_simulation/brownian.py
@@ -16,6 +16,12 @@ from base import plot2d, plotocc
 
 class Brownian(plot2d):
 
+    #
+    #  Licensing:
+    #
+    #    This code is distributed under the GNU LGPL license.
+    #
+
     def __init__(self):
         plot2d.__init__(self)
 
@@ -24,17 +30,14 @@ class Brownian(plot2d):
         n = 1001
         d = 10.0
         t = 1.0
-        x = self.brownian_motion_simulation(m, n, d, t)
-        self.brownian_motion_display(m, n, x)
-
+        pnt = self.brownian_motion_simulation(m, n, d, t)
         dsq = self.brownian_displacement_simulation(k, n, m, d, t)
+        self.brownian_motion_display(m, n, pnt)
         self.brownian_displacement_display(k, n, m, d, t, dsq)
 
     def brownian_motion_simulation(self, m=2, n=1001, d=10.0, t=1.0):
-
-        # *****************************************************************************80
         #
-        # BROWNIAN_MOTION_SIMULATION simulates Brownian motion.
+        #  BROWNIAN_MOTION_SIMULATION simulates Brownian motion.
         #
         #  Discussion:
         #
@@ -44,32 +47,12 @@ class Brownian(plot2d):
         #    Thanks to Joerg Peter Pfannmoeller for pointing out a missing factor
         #    of M in the stepsize calculation, 23 April 2018.
         #
-        #  Licensing:
-        #
-        #    This code is distributed under the GNU LGPL license.
-        #
-        #  Modified:
-        #
-        #    10 June 2018
-        #
-        #  Author:
-        #
-        #    John Burkardt
-        #
         #  Parameters:
         #
         #    Input, integer M, the spatial dimension.
-        #    This defaults to 2.
-        #
         #    Input, integer N, the number of time steps to take, plus 1.
-        #    This defaults to 1001.
-        #
         #    Input, real D, the diffusion coefficient.
-        #    This defaults to 10.0.
-        #
         #    Input, real T, the total time.
-        #    This defaults to 1.0
-        #
         #    Output, real X(M,N), the initial position at time 0.0, and
         #    the N-1 successive locations of the particle.
         #
@@ -97,91 +80,7 @@ class Brownian(plot2d):
 
         return x
 
-    def brownian_motion_display(self, m, n, x):
-
-        # *****************************************************************************80
-        #
-        # BROWNIAN_MOTION_DISPLAY displays successive Brownian motion positions.
-        #
-        #  Licensing:
-        #
-        #    This code is distributed under the GNU LGPL license.
-        #
-        #  Modified:
-        #
-        #    26 April 2018
-        #
-        #  Author:
-        #
-        #    John Burkardt
-        #
-        #  Parameters:
-        #
-        #    Input, integer M, the spatial dimension.
-        #    M should be 1, 2 or 3.
-        #
-        #    Input, integer N, the number of time steps.
-        #
-        #    Input, real X(M,N), the particle positions.
-        #
-
-        if (m == 1):
-
-            y = np.linspace(0, n - 1, n) / float(n - 1)
-            plt.plot(x[0, :], y[:], 'b', linewidth=2)
-            plt.plot(x[0, 0], y[0], 'g.', markersize=35)
-            plt.plot(x[0, n - 1], y[n - 1], 'r.', markersize=35)
-            plt.grid(True)
-            plt.xlabel('<--X-->')
-            plt.ylabel('<--Time-->')
-            plt.title('Brownian motion simulation in 1D')
-            filename = 'motion_' + str(m) + 'd.png'
-            plt.savefig(filename)
-
-        elif (m == 2):
-
-            plt.plot(x[0, :], x[1, :], 'b', LineWidth=2)
-            plt.plot(x[0, 0], x[1, 0], 'g.', markersize=35)
-            plt.plot(x[0, n - 1], x[1, n - 1], 'r.', markersize=35)
-            plt.grid(True)
-            plt.xlabel('<--X-->')
-            plt.ylabel('<--Y-->')
-            plt.title('Brownian motion simulation in 2D')
-            plt.axis('equal')
-            filename = 'motion_' + str(m) + 'd.png'
-            plt.savefig(filename)
-
-        #  May I just say that I am struck by the inconsistency between 2D and 3D
-        elif (m == 3):
-
-            fig = plt.figure()
-            ax = fig.add_subplot(111, projection='3d')
-            ax.plot(x[0, :], x[1, :], x[2, :], 'b', linewidth=2)
-            ax.scatter(x[0, 0], x[1, 0], x[2, 0], c='g', marker='o', s=100)
-            ax.scatter(x[0, n - 1], x[1, n - 1],
-                       x[2, n - 1], c='r', marker='o', s=100)
-            ax.grid(True)
-            ax.set_xlabel('<--X-->')
-            ax.set_ylabel('<--Y-->')
-            ax.set_zlabel('<--Z-->')
-            plt.title('Brownian motion simulation in 3D')
-            filename = 'motion_' + str(m) + 'd.png'
-            plt.savefig(filename)
-
-        else:
-
-            print('')
-            print('BROWNIAN_MOTION_DISPLAY - Fatal error!')
-            print('  Cannot display data except for M = 1, 2, 3.')
-            exit('BROWNIAN_MOTION_DISPLAY - Fatal error!')
-
-        print('')
-        print('  Plot saved as "%s".' % (filename))
-
-    def brownian_displacement_simulation(self, k=20, n=1001, m=2, d=10.0,
-                                         t=1.0):
-
-        # *****************************************************************************80
+    def brownian_displacement_simulation(self, k=20, n=1001, m=2, d=10.0, t=1.0):
         #
         # BROWNIAN_DISPLACEMENT_SIMULATION simulates Brownian displacement.
         #
@@ -194,47 +93,82 @@ class Brownian(plot2d):
         #    particle from the starting point, repeating this calculation
         #    several times.
         #
-        #  Licensing:
-        #
-        #    This code is distributed under the GNU LGPL license.
-        #
-        #  Modified:
-        #
-        #    10 June 2018
-        #
-        #  Author:
-        #
-        #    John Burkardt
-        #
         #  Parameters:
         #
         #    Input, integer K, the number of repetitions.
-        #    The default is 20
-        #
         #    Input, integer N, the number of time steps to take, plus 1.
-        #    This might be 1001.
-        #
         #    Input, integer M, the spatial dimension.  Typically, this is 2.
-        #
-        #    Input, real D, the diffusion coefficient.  This might be 10.0.
-        #    Computationally, this is simply a scale factor between time and space.
-        #
+        #    Input, real D, the diffusion coefficient.
+        #       This might be 10.0.
+        #       Computationally, this is simply a scale factor between time and space.
         #    Input, real T, the total time, which defaults to 1.0.
-        #
         #    Output, real DSQ(K,N), the displacements over time for each repetition.
         #    DSQ(:,1) is 0.0, because we include the displacement at the initial time.
         #
 
         dsq = np.zeros([k, n])
         for i in range(0, k):
-            x = self.brownian_motion_simulation(m, n, d, t)
-            dsq[i, 0:n] = np.sum(x[0:m, 0:n] ** 2, 0)
+            pnt = self.brownian_motion_simulation(m, n, d, t)
+            dsq[i, 0:n] = np.sum(pnt[0:m, 0:n] ** 2, 0)
 
         return dsq
 
-    def brownian_displacement_display(self, k, n, m, d, t, dsq):
+    def brownian_motion_display(self, m, n, x):
+        #
+        # BROWNIAN_MOTION_DISPLAY displays successive Brownian motion positions.
+        #
+        #  Parameters:
+        #
+        #    Input, integer M, the spatial dimension.
+        #       M should be 1, 2 or 3.
+        #    Input, integer N, the number of time steps.
+        #    Input, real X(M,N), the particle positions.
+        #
+        
+        self.new_2Dfig(aspect="auto")
 
-        # *****************************************************************************80
+        if (m == 1):
+            filename = 'motion_' + str(m) + 'd.png'
+            y = np.linspace(0, n - 1, n) / float(n - 1)
+            self.axs.plot(x[0, :], y[:], 'b', linewidth=2)
+            self.axs.plot(x[0, 0], y[0], 'g.', markersize=35)
+            self.axs.plot(x[0, n - 1], y[n - 1], 'r.', markersize=35)
+            self.axs.set_xlabel('<--X-->')
+            self.axs.set_ylabel('<--Time-->')
+            self.axs.set_title('Brownian motion simulation in 1D')
+            self.SavePng(filename)
+            
+        elif (m == 2):
+            filename = 'motion_' + str(m) + 'd.png'
+            self.axs.plot(x[0, :], x[1, :], 'b', LineWidth=2)
+            self.axs.plot(x[0, 0], x[1, 0], 'g.', markersize=35)
+            self.axs.plot(x[0, n - 1], x[1, n - 1], 'r.', markersize=35)
+            self.axs.set_xlabel('<--X-->')
+            self.axs.set_ylabel('<--Y-->')
+            self.axs.set_title('Brownian motion simulation in 2D')
+            self.SavePng(filename)
+
+        elif (m == 3):
+            filename = 'motion_' + str(m) + 'd.png'
+            self.new_3Dfig()
+            self.axs.plot(x[0, :], x[1, :], x[2, :], 'b', linewidth=2)
+            self.axs.scatter(x[0, 0], x[1, 0], x[2, 0], c='g', marker='o', s=100)
+            self.axs.scatter(x[0, n - 1], x[1, n - 1],
+                       x[2, n - 1], c='r', marker='o', s=100)
+            self.axs.set_title('Brownian motion simulation in 3D')
+            self.SavePng(filename)
+
+        else:
+
+            print('')
+            print('BROWNIAN_MOTION_DISPLAY - Fatal error!')
+            print('  Cannot display data except for M = 1, 2, 3.')
+            exit('BROWNIAN_MOTION_DISPLAY - Fatal error!')
+
+        print('')
+        print('  Plot saved as "%s".' % (filename))
+
+    def brownian_displacement_display(self, k, n, m, d, t, dsq):
         #
         # BROWNIAN_DISPLACEMENT_DISPLAY displays average Brownian motion displacement.
         #
@@ -243,32 +177,17 @@ class Brownian(plot2d):
         #    Thanks to Feifei Xu for pointing out a missing factor of 2 in the
         #    displacement calculation.
         #
-        #  Licensing:
-        #
-        #    This code is distributed under the GNU LGPL license.
-        #
-        #  Modified:
-        #
-        #    10 June 2018
-        #
-        #  Author:
-        #
-        #    John Burkardt
-        #
         #  Parameters:
         #
         #    Input, integer K, the number of repetitions.
-        #
         #    Input, integer N, the number of time steps.
-        #
         #    Input, integer M, the spatial dimension.
-        #
         #    Input, real D, the diffusion coefficient.
-        #
         #    Input, real T, the total time.
-        #
         #    Input, real DSQ(K,N), the displacements over time for each repetition.
         #
+        
+        self.new_2Dfig(aspect="auto")
 
         #  Get the T values.
         tvec = np.linspace(0, t, n)
@@ -276,23 +195,22 @@ class Brownian(plot2d):
         #  Select 5 random trajectories for display.
         for s in range(0, 5):
             i = int(k * np.random.rand(1))
-            plt.plot(tvec, dsq[i, :], 'b-')
+            self.axs.plot(tvec, dsq[i, :], 'b-')
 
         #  Display the average displacement.
         dsq_ave = np.sum(dsq, 0) / float(k)
-        plt.plot(tvec, dsq_ave, 'r-', linewidth=2)
+        self.axs.plot(tvec, dsq_ave, 'r-', linewidth=2)
 
         #  Display the ideal displacment.
         dsq_ideal = 2.0 * m * d * tvec
-        plt.plot(tvec, dsq_ideal, 'k-', linewidth=3)
+        self.axs.plot(tvec, dsq_ideal, 'k-', linewidth=3)
 
-        plt.grid(True)
-        plt.xlabel('<--T-->')
-        plt.ylabel('<--D^2-->')
-        plt.title('Squared displacement (Red), Predicted (Black), Samples (Blue)')
+        self.axs.set_xlabel('<--T-->')
+        self.axs.set_ylabel('<--D^2-->')
+        self.axs.set_title('Squared displacement (Red), Predicted (Black), Samples (Blue)')
 
         filename = 'displacement_' + str(m) + '.png'
-        plt.savefig(filename)
+        self.SavePng(filename)
 
         print('')
         print('  Plot saved as "%s".' % (filename))
