2dc3e32d5baa2c6e7bffb9cec4490259bbdd5753
tnakaicode <tnakaicode@gmail.com>
2021-01-16 10:53:43 +0900

    ranlib

--- ranlib/r4_exponential_sample.py
+++ ranlib/r4_exponential_sample.py
@@ -1,88 +1,104 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 #
-def r4_exponential_sample ( lam ):
 
-#*****************************************************************************80
-#
-## R4_EXPONENTIAL_SAMPLE samples the exponential PDF.
-#
-#  Discussion:
-#
-#    Note that the parameter LAM is a multiplier.  In some formulations,
-#    it is used as a divisor instead.
-#
-#  Licensing:
-#
-#    This code is distributed under the GNU LGPL license.
-#
-#  Modified:
-#
-#    03 September 2018
-#
-#  Author:
-#
-#    John Burkardt
-#
-#  Parameters:
-#
-#    Input, real LAM, the parameter of the PDF.
-#
-#    Output, real VALUE, a sample of the PDF.
-#
-  import numpy as np
-  from r4_uni_01 import r4_uni_01
+import numpy as np
+import matplotlib.pyplot as plt
+import platform
+import time
+import sys
+import os
+import math
+from mpl_toolkits.mplot3d import Axes3D
+from sys import exit
 
-  r = r4_uni_01 ( );
+sys.path.append(os.path.join("../"))
+from base import plot2d, plotocc
+from timestamp.timestamp import timestamp
 
-  value = - np.log ( r ) * lam
+from rnglib.r4_uni_01 import r4_uni_01
 
-  return value
 
-def r4_exponential_sample_test ( ):
+def r4_exponential_sample(lam):
 
-#*****************************************************************************80
-#
-## R4_EXPONENTIAL_TEST tests R4_EXPONENTIAL.
-#
-#  Licensing:
-#
-#    This code is distributed under the GNU LGPL license.
-#
-#  Modified:
-#
-#    03 September 2018
-#
-#  Author:
-#
-#    John Burkardt
-#
-  import platform
+    # *****************************************************************************80
+    #
+    # R4_EXPONENTIAL_SAMPLE samples the exponential PDF.
+    #
+    #  Discussion:
+    #
+    #    Note that the parameter LAM is a multiplier.  In some formulations,
+    #    it is used as a divisor instead.
+    #
+    #  Licensing:
+    #
+    #    This code is distributed under the GNU LGPL license.
+    #
+    #  Modified:
+    #
+    #    03 September 2018
+    #
+    #  Author:
+    #
+    #    John Burkardt
+    #
+    #  Parameters:
+    #
+    #    Input, real LAM, the parameter of the PDF.
+    #
+    #    Output, real VALUE, a sample of the PDF.
+    #
 
-  test_num = 20
+    r = r4_uni_01()
 
-  print ( '' )
-  print ( 'R4_EXPONENTIAL_TEST' )
-  print ( '  Python version: %s' % ( platform.python_version ( ) ) )
-  print ( '  R4_EXPONENTIAL samples the exponential distribution.' )
-  print ( '' )
+    value = - np.log(r) * lam
 
-  lam = 0.5
+    return value
 
-  for test in range ( 0, test_num ):
 
-    x = r4_exponential_sample ( lam )
-    print ( '  %f' % ( x ) )
+def r4_exponential_sample_test():
+
+    # *****************************************************************************80
+    #
+    # R4_EXPONENTIAL_TEST tests R4_EXPONENTIAL.
+    #
+    #  Licensing:
+    #
+    #    This code is distributed under the GNU LGPL license.
+    #
+    #  Modified:
+    #
+    #    03 September 2018
+    #
+    #  Author:
+    #
+    #    John Burkardt
+    #
+
+    test_num = 20
+
+    print('')
+    print('R4_EXPONENTIAL_TEST')
+    print('  Python version: %s' % (platform.python_version()))
+    print('  R4_EXPONENTIAL samples the exponential distribution.')
+    print('')
+
+    lam = 0.5
+
+    for test in range(0, test_num):
+
+        x = r4_exponential_sample(lam)
+        print('  %f' % (x))
 #
 #  Terminate.
 #
-  print ( '' )
-  print ( 'R4_EXPONENTIAL_TEST' )
-  print ( '  Normal end of execution.' )
-  return
+    print('')
+    print('R4_EXPONENTIAL_TEST')
+    print('  Normal end of execution.')
+    return
 
-if ( __name__ == '__main__' ):
-  from timestamp import timestamp
-  timestamp ( )
-  r4_exponential_sample_test ( )
-  timestamp ( )
 
+if (__name__ == '__main__'):
+    from timestamp import timestamp
+    timestamp()
+    r4_exponential_sample_test()
+    timestamp()
--- ranlib/ranlib_test.py
+++ ranlib/ranlib_test.py
@@ -15,6 +15,40 @@ sys.path.append(os.path.join("../"))
 from base import plot2d, plotocc
 from timestamp.timestamp import timestamp
 
+from r8lib.r8vec_covariance import r8vec_covariance_test
+from r4lib.r4lib import r4vec_covariance_test
+
+from ranlib.r4_exponential_sample import r4_exponential_sample_test
+from ranlib.r8_exponential_sample import r8_exponential_sample_test
+
+from ranlib.genbet import genbet_test
+from ranlib.genchi import genchi_test
+from ranlib.genexp import genexp_test
+from ranlib.genf import genf_test
+from ranlib.gengam import gengam_test
+from ranlib.genmn import genmn_test
+from ranlib.genmul import genmul_test
+from ranlib.gennch import gennch_test
+from ranlib.gennf import gennf_test
+from ranlib.gennor import gennor_test
+from ranlib.genprm import genprm_test
+from ranlib.genunf import genunf_test
+from ranlib.ignbin import ignbin_test
+from ranlib.ignnbn import ignnbn_test
+from ranlib.ignpoi import ignpoi_test
+from ranlib.ignuin import ignuin_test
+from ranlib.lennob import lennob_test
+from ranlib.low_level_test import low_level_test
+from ranlib.phrtsd import phrtsd_test
+from ranlib.prcomp import prcomp_test
+from ranlib.setcov import setcov_test
+from ranlib.sexpo import sexpo_test
+from ranlib.sgamma import sgamma_test
+from ranlib.snorm import snorm_test
+from ranlib.spofa import spofa_test
+from ranlib.stats import stats_test
+from ranlib.trstat import trstat_test
+
 
 def ranlib_test():
 
@@ -34,41 +68,6 @@ def ranlib_test():
     #
     #    John Burkardt
     #
-    import platform
-
-    from ranlib.genbet import genbet_test
-    from ranlib.genchi import genchi_test
-    from ranlib.genexp import genexp_test
-    from ranlib.genf import genf_test
-    from ranlib.gengam import gengam_test
-    from ranlib.genmn import genmn_test
-    from ranlib.genmul import genmul_test
-    from ranlib.gennch import gennch_test
-    from ranlib.gennf import gennf_test
-    from ranlib.gennor import gennor_test
-    from ranlib.genprm import genprm_test
-    from ranlib.genunf import genunf_test
-    from ranlib.ignbin import ignbin_test
-    from ranlib.ignnbn import ignnbn_test
-    from ranlib.ignpoi import ignpoi_test
-    from ranlib.ignuin import ignuin_test
-    #from ranlib.initialize import initialize
-    from ranlib.lennob import lennob_test
-    from ranlib.low_level_test import low_level_test
-    from ranlib.phrtsd import phrtsd_test
-    from ranlib.prcomp import prcomp_test
-    from ranlib.r4_exp import r4_exp_test
-    from ranlib.r4_exponential_sample import r4_exponential_sample_test
-    from ranlib.r4vec_covariance import r4vec_covariance_test
-    from ranlib.r8_exponential_sample import r8_exponential_sample_test
-    from ranlib.r8vec_covariance import r8vec_covariance_test
-    from ranlib.setcov import setcov_test
-    from ranlib.sexpo import sexpo_test
-    from ranlib.sgamma import sgamma_test
-    from ranlib.snorm import snorm_test
-    from ranlib.spofa import spofa_test
-    from ranlib.stats import stats_test
-    from ranlib.trstat import trstat_test
 
     phrase = 'Randomizer'
 
@@ -99,11 +98,6 @@ def ranlib_test():
     low_level_test()
     phrtsd_test()
     prcomp_test()
-    r4_exp_test()
-    r4_exponential_sample_test()
-    r4vec_covariance_test()
-    r8_exponential_sample_test()
-    r8vec_covariance_test()
     setcov_test()
     sexpo_test()
     sgamma_test()
