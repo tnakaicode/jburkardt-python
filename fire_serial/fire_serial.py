c7545a882a459ce5f3570c0ba2c594782921ac6e
tnakaicode <tnakaicode@gmail.com>
2022-03-26 01:46:08 +0900

    dire serial

--- fire_serial/fire_serial.py
+++ fire_serial/fire_serial.py
@@ -1,5 +1,30 @@
 #! /usr/bin/env python3
 #
+
+import numpy as np
+import matplotlib.pyplot as plt
+import platform
+import time
+import sys
+import os
+import math
+from mpl_toolkits.mplot3d import Axes3D
+from sys import exit
+
+sys.path.append(os.path.join("../"))
+from base import plot2d, plotocc
+from timestamp.timestamp import timestamp
+
+from i4lib.i4vec_print import i4vec_print
+from i4lib.i4mat_print import i4mat_print, i4mat_print_some
+from i4lib.i4_uniform_ab import i4_uniform_ab
+from r8lib.r8vec_print import r8vec_print, r8vec_print_some, r8vec_transpose_print
+from r8lib.r8mat_print import r8mat_print, r8mat_print_some
+from r8lib.r8mat_write import r8mat_write
+from r8lib.r8vec_transpose import r8vec_transpose_print
+from r8lib.r8mat_transpose import r8mat_transpose_print, r8mat_transpose_print_some
+from r8lib.r8 import r8_uniform_01
+
 UNBURNT = 0
 SMOLDERING = 1
 BURNING = 2
@@ -366,328 +391,6 @@ def get_percent_burned(forest_size, forest):
     percent = float(total) / float(forest_size) / float(forest_size)
 
     return percent
-
-
-def i4_uniform_ab(a, b, seed):
-
-    # *****************************************************************************80
-    #
-    # I4_UNIFORM_AB returns a scaled pseudorandom I4.
-    #
-    #  Discussion:
-    #
-    #    The pseudorandom number will be scaled to be uniformly distributed
-    #    between A and B.
-    #
-    #  Licensing:
-    #
-    #    This code is distributed under the GNU LGPL license.
-    #
-    #  Modified:
-    #
-    #    05 April 2013
-    #
-    #  Author:
-    #
-    #    John Burkardt
-    #
-    #  Reference:
-    #
-    #    Paul Bratley, Bennett Fox, Linus Schrage,
-    #    A Guide to Simulation,
-    #    Second Edition,
-    #    Springer, 1987,
-    #    ISBN: 0387964673,
-    #    LC: QA76.9.C65.B73.
-    #
-    #    Bennett Fox,
-    #    Algorithm 647:
-    #    Implementation and Relative Efficiency of Quasirandom
-    #    Sequence Generators,
-    #    ACM Transactions on Mathematical Software,
-    #    Volume 12, Number 4, December 1986, pages 362-376.
-    #
-    #    Pierre L'Ecuyer,
-    #    Random Number Generation,
-    #    in Handbook of Simulation,
-    #    edited by Jerry Banks,
-    #    Wiley, 1998,
-    #    ISBN: 0471134031,
-    #    LC: T57.62.H37.
-    #
-    #    Peter Lewis, Allen Goodman, James Miller,
-    #    A Pseudo-Random Number Generator for the System/360,
-    #    IBM Systems Journal,
-    #    Volume 8, Number 2, 1969, pages 136-143.
-    #
-    #  Parameters:
-    #
-    #    Input, integer A, B, the minimum and maximum acceptable values.
-    #
-    #    Input, integer SEED, a seed for the random number generator.
-    #
-    #    Output, integer C, the randomly chosen integer.
-    #
-    #    Output, integer SEED, the updated seed.
-    #
-    from math import floor
-    from sys import exit
-
-    i4_huge = 2147483647
-
-    seed = floor(seed)
-
-    seed = (seed % i4_huge)
-
-    if (seed < 0):
-        seed = seed + i4_huge
-
-    if (seed == 0):
-        print('')
-        print('I4_UNIFORM_AB - Fatal error!')
-        print('  Input SEED = 0!')
-        exit('I4_UNIFORM_AB - Fatal error!')
-
-    k = floor(seed / 127773)
-
-    seed = 16807 * (seed - k * 127773) - k * 2836
-
-    if (seed < 0):
-        seed = seed + i4_huge
-
-    r = seed * 4.656612875E-10
-#
-#  Scale R to lie between A-0.5 and B+0.5.
-#
-    a = round(a)
-    b = round(b)
-
-    r = (1.0 - r) * (min(a, b) - 0.5) \
-        + r * (max(a, b) + 0.5)
-#
-#  Use rounding to convert R to an integer between A and B.
-#
-    value = round(r)
-
-    value = max(value, min(a, b))
-    value = min(value, max(a, b))
-    value = int(value)
-
-    return value, seed
-
-
-def i4_uniform_ab_test():
-
-    # *****************************************************************************80
-    #
-    # I4_UNIFORM_AB_TEST tests I4_UNIFORM_AB.
-    #
-    #  Licensing:
-    #
-    #    This code is distributed under the GNU LGPL license.
-    #
-    #  Modified:
-    #
-    #    27 October 2014
-    #
-    #  Author:
-    #
-    #    John Burkardt
-    #
-    import platform
-
-    a = -100
-    b = 200
-    seed = 123456789
-
-    print('')
-    print('I4_UNIFORM_AB_TEST')
-    print('  Python version: %s' % (platform.python_version()))
-    print('  I4_UNIFORM_AB computes pseudorandom values')
-    print('  in an interval [A,B].')
-    print('')
-    print('  The lower endpoint A = %d' % (a))
-    print('  The upper endpoint B = %d' % (b))
-    print('  The initial seed is %d' % (seed))
-    print('')
-
-    for i in range(1, 21):
-        [j, seed] = i4_uniform_ab(a, b, seed)
-        print('  %8d  %8d' % (i, j))
-#
-#  Terminate.
-#
-    print('')
-    print('I4_UNIFORM_AB_TEST:')
-    print('  Normal end of execution.')
-    return
-
-
-def r8_uniform_01(seed):
-
-    # *****************************************************************************80
-    #
-    # R8_UNIFORM_01 returns a unit pseudorandom R8.
-    #
-    #  Discussion:
-    #
-    #    This routine implements the recursion
-    #
-    #      seed = 16807 * seed mod ( 2^31 - 1 )
-    #      r8_uniform_01 = seed / ( 2^31 - 1 )
-    #
-    #    The integer arithmetic never requires more than 32 bits,
-    #    including a sign bit.
-    #
-    #    If the initial seed is 12345, then the first three computations are
-    #
-    #      Input     Output      R8_UNIFORM_01
-    #      SEED      SEED
-    #
-    #         12345   207482415  0.096616
-    #     207482415  1790989824  0.833995
-    #    1790989824  2035175616  0.947702
-    #
-    #  Licensing:
-    #
-    #    This code is distributed under the GNU LGPL license.
-    #
-    #  Modified:
-    #
-    #    17 March 2013
-    #
-    #  Author:
-    #
-    #    John Burkardt
-    #
-    #  Reference:
-    #
-    #    Paul Bratley, Bennett Fox, Linus Schrage,
-    #    A Guide to Simulation,
-    #    Second Edition,
-    #    Springer, 1987,
-    #    ISBN: 0387964673,
-    #    LC: QA76.9.C65.B73.
-    #
-    #    Bennett Fox,
-    #    Algorithm 647:
-    #    Implementation and Relative Efficiency of Quasirandom
-    #    Sequence Generators,
-    #    ACM Transactions on Mathematical Software,
-    #    Volume 12, Number 4, December 1986, pages 362-376.
-    #
-    #    Pierre L'Ecuyer,
-    #    Random Number Generation,
-    #    in Handbook of Simulation,
-    #    edited by Jerry Banks,
-    #    Wiley, 1998,
-    #    ISBN: 0471134031,
-    #    LC: T57.62.H37.
-    #
-    #    Peter Lewis, Allen Goodman, James Miller,
-    #    A Pseudo-Random Number Generator for the System/360,
-    #    IBM Systems Journal,
-    #    Volume 8, Number 2, 1969, pages 136-143.
-    #
-    #  Parameters:
-    #
-    #    Input, integer SEED, the integer "seed" used to generate
-    #    the output random number.  SEED should not be 0.
-    #
-    #    Output, real R, a random value between 0 and 1.
-    #
-    #    Output, integer SEED, the updated seed.  This would
-    #    normally be used as the input seed on the next call.
-    #
-    from math import floor
-    from sys import exit
-
-    i4_huge = 2147483647
-
-    seed = floor(seed)
-
-    seed = (seed % i4_huge)
-
-    if (seed < 0):
-        seed = seed + i4_huge
-
-    if (seed == 0):
-        print('')
-        print('R8_UNIFORM_01 - Fatal error!')
-        print('  Input SEED = 0!')
-        exit('R8_UNIFORM_01 - Fatal error!')
-
-    k = floor(seed / 127773)
-
-    seed = 16807 * (seed - k * 127773) - k * 2836
-
-    if (seed < 0):
-        seed = seed + i4_huge
-
-    r = seed * 4.656612875E-10
-
-    return r, seed
-
-
-def r8_uniform_01_test():
-
-    # *****************************************************************************80
-    #
-    # R8_UNIFORM_01_TEST tests R8_UNIFORM_01.
-    #
-    #  Licensing:
-    #
-    #    This code is distributed under the GNU LGPL license.
-    #
-    #  Modified:
-    #
-    #    26 July 2014
-    #
-    #  Author:
-    #
-    #    John Burkardt
-    #
-    import platform
-
-    print('')
-    print('R8_UNIFORM_01_TEST')
-    print('  Python version: %s' % (platform.python_version()))
-    print('  R8_UNIFORM_01 produces a sequence of random values.')
-
-    seed = 123456789
-
-    print('')
-    print('  Using random seed %d' % (seed))
-
-    print('')
-    print('  SEED  R8_UNIFORM_01(SEED)')
-    print('')
-    for i in range(0, 10):
-        seed_old = seed
-        x, seed = r8_uniform_01(seed)
-        print('  %12d  %14f' % (seed, x))
-
-    print('')
-    print('  Verify that the sequence can be restarted.')
-    print('  Set the seed back to its original value, and see that')
-    print('  we generate the same sequence.')
-
-    seed = 123456789
-    print('')
-    print('  SEED  R8_UNIFORM_01(SEED)')
-    print('')
-
-    for i in range(0, 10):
-        seed_old = seed
-        x, seed = r8_uniform_01(seed)
-        print('  %12d  %14f' % (seed, x))
-#
-#  Terminate.
-#
-    print('')
-    print('R8_UNIFORM_01_TEST')
-    print('  Normal end of execution.')
-    return
 
 
 def tree_ignite(forest_size, forest, i_ignite, j_ignite):
