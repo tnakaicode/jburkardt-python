a718f8f87ccce14fbeb385395a044c0802ebc0d1
tnakaicode <tnakaicode@gmail.com>
2022-03-26 10:04:04 +0900

    note

--- cobweb_plot/cobweb_plot.py
+++ cobweb_plot/cobweb_plot.py
@@ -15,12 +15,6 @@ sys.path.append(os.path.join("../"))
 from base import plot2d, plotocc
 from timestamp.timestamp import timestamp
 
-from i4lib.i4vec_print import i4vec_print
-from i4lib.i4mat_print import i4mat_print
-from r8lib.r8vec_print import r8vec_print
-from r8lib.r8mat_print import r8mat_print, r8mat_print_some
-from r8lib.r8mat_write import r8mat_write
-
 
 def cobweb_plot(f, x0, N, a=0, b=1):
 
